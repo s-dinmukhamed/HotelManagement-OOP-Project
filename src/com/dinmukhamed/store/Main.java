158b46879efe1080dec881487f757a5bcd582c54
Dimash <dimahssydykov@gmail.com>
2025-12-10 09:41:48 +0500

    Describing features

--- src/com/dinmukhamed/store/Main.java
+++ src/com/dinmukhamed/store/Main.java
@@ -3,5 +3,8 @@ package com.dinmukhamed.store;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello ClothingStore");
+        System.out.println("This system allows customers to browse available clothes by size, color, and category.");
+        System.out.println("The system supports creating and managing orders, including payment and delivery options.");
+        System.out.println("Store admins can add new items, update prices, and track sales statistics.");
     }
 }
